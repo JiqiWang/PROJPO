4405f0bfd58a5dc7ac601dc28ebbf312ece406f5
gui-alm <galmeidap@live.com.pt>
2021-10-25 01:04:48 +0100

    Added Partner class, not implemented yet.

---
+++ project/ggc-core/src/ggc/partners/Partner.java
@@ -0,0 +1,32 @@
+package ggc.partners;
+
+import java.io.Serializable;
+
+public class Partner implements Serializable {
+    
+    private String _id;
+    private String _name;
+    private String _address;
+
+    // add status (use design pattern)
+
+    private double _points;
+
+    // add other parameters to these methods
+
+    public void registerPartner(){
+        // implement
+    }
+
+    public void showPartnerAquisitions(){
+        // implement
+    }
+
+    public void showPartnerSales(){
+        // implement
+    }
+
+    public void togglePartnerNotifications(){
+        // implement, not needed for this part of the project
+    }
+}
